956fadc57116f169aba194ec5f70857ff3386e5e
abmann18 <ashley.b.mann@gmail.com>
2015-04-09 12:39:15 -0400

    Final commit.

--- js/myLibrary.js
+++ js/myLibrary.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 //when you click the left button, the text "I'm right" will appear
 
 	$(".leftbutton").click(function(){
-		$(".lefttext").text("I'm right!")
+		$(".lefttext").text("I'm right!");
 	});
 
 
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
 	$(".rightbutton").click(function(){
 		$(".righttext").text("I'm right!")
-			$(".lefttext").text("Left button")
+			$(".lefttext").text("Left button");
 	});
 
 
@@ -19,6 +19,19 @@ $(document).ready(function(){
 
 	$(".leftbutton").click(function(){
 		$(".lefttext").text("I'm right!")
-			$(".righttext").text("Right button")
+			$(".righttext").text("Right button");
 	});
+
+
+//when you hover over this banner, the text below will appear
+	$(".banner").hover(function(){
+		$(".bannertext").toggle();
+	});
+
+
+//when you hover over the banner, everything will disappear and new text will appear (not sure why this isn't working and why it causes the 4th event to not work)	
+
+	// $(".container").hide(function(){
+	// 	alert("Everything disappeared");
+	// }
 });
